0acf8e0cce903232311f74828877e2598bd498b9
Nic Zern <nic.zern@pcc.edu>
2018-10-24 18:15:58 -0700

    Added main to print hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Author: Cara Tang and Nic Zern
+ * Version: 10.24.18
+ */
 public class HelloWorld {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello, world.");
+    }
 }
