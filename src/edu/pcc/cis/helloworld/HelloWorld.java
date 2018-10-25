de955fb09b5f7f0ce6c97a46314d9142fbdf0fc7
Nic Zern <nic.zern@pcc.edu>
2018-10-24 18:17:49 -0700

    print date added

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,5 @@
 package edu.pcc.cis.helloworld;
-
+import java.util.Date;
 /**
  * Author: Cara Tang and Nic Zern
  * Version: 10.24.18
@@ -8,5 +8,6 @@ public class HelloWorld {
     public static void main (String[] args)
     {
         System.out.println("Hello, world.");
+        System.out.println(new Date());
     }
 }
